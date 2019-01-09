60ba470cbb2cf9689703a5c614cca96a80c833b3
Manoj Chafekar <manojchafekar@gmail.com>
2019-01-10 00:58:20 +0530

    raniumtestCOde

--- main.js
+++ main.js
@@ -18,18 +18,35 @@ application.controller("formcontroller", function($scope, $http) {
 			url : "https://api.nasa.gov/neo/rest/v1/feed?start_date="+start_date+"&end_date="+end_date+"&detailed=false&api_key=WegOSxggnyy9Ym61JHXGsFjMKdAoNbJIpQoxpiEM",
 			data: $scope.insert,
 		}).then(function (response){
-			$scope.links = response.data.near_earth_objects;
-			$scope.rtg=JSON.stringify($scope.links);
 			
-			console.log(location);
+			$scope.links = response.data.near_earth_objects;
+			rtg = $scope.links;
+			count = Object.keys(rtg).length;
+			console.log(rtg);
+
+			for (var i = 0; i <= count; i++) {
+				//console.log(i);
+				console.log(rtg[i]);
+				
+			}
 			
 
-			
+			/*for (i in rtg) {
+				dates = rtg[i];
+				console.log(dates);
+			  x += "<h2>" + myObj.cars[i].name + "</h2>";
+			  for (j in myObj.cars[i].models) {
+			    x += myObj.cars[i].models[j] + "<br>";
+			  }
+			}	*/
+
+
 		});
 
 
-		console.log('start_date',start_date);
-		console.log('start_date',end_date);
+
+		/*console.log('start_date',start_date);
+		console.log('start_date',end_date);*/
 	}
 });
 
